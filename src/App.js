3ae31ca563c2538ebc1ba4ab9f37f255e86b51c4
Abdelnacerr <abdelnacerr@gmail.com>
2021-08-04 12:47:42 +1000

    reducers and sags h

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+// bring Provider from react-redux, it's the bridge for connecting react to redux
+import { Provider } from 'react-redux'
+
+// Bring the redux store too
+import store from './redux/store'
+
+// Components
+import Todo from './components/Todo'
+
+const App = () => {
+	return (
+		// Register your redux Provider here
+		<Provider store={store}>
+			<Todo />
+		</Provider>
+	)
 }
-
-export default App;
+export default App
